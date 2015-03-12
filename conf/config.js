fc7396f75659d3b30479735285bef3db8f28315c
lyhiving <lyhiving@gmail.com>
2015-03-13 04:23:28 +0800

    修改默认端口

--- conf/config.js
+++ conf/config.js
@@ -7,7 +7,7 @@ var path = require("path"),
 
 config = {
     name: '简易谷歌',
-    port: '5555',
+    port: '4040',
     language: 'zh_CN',//生产环境下使用zh_CN
     userAgent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ' +
         '(KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36',
